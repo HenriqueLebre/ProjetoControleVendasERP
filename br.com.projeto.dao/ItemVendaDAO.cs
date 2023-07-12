79d237e79ca25d9af1f8f70bc8dc5cae5ed23a0a
henriqueLebre <henrique_lebre@hotmail.com>
2023-07-11 21:28:33 -0300

    v0.0.22 - Consulta por item para retornar detalhes

--- br.com.projeto.dao/ItemVendaDAO.cs
+++ br.com.projeto.dao/ItemVendaDAO.cs
@@ -3,6 +3,7 @@ using ProjetoControleVendas.br.com.projeto.conexao;
 using ProjetoControleVendas.br.com.projeto.model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +46,50 @@ namespace ProjetoControleVendas.br.com.projeto.dao
 
                 MessageBox.Show("Item cadastrado com sucesso!");
                 conn.Close();
+
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Obtivemos um erro, valide as informações: " + erro);
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region listaItemPorVenda
+
+        public DataTable listaItemPorVenda(int venda_id)
+        {
+            try
+            {
+
+                DataTable tabelItens = new DataTable();
+
+
+                string cCmdSql = @"SELECT i.id as 'Código',
+                                          p.descricao as 'Descrição',
+                                          i.qtd as 'Quantidade',
+                                          p.preco as 'Preço',
+                                          i.subtotal as 'Subtotal'
+                                    FROM tb_itensvendas as i join
+                                         tb_produtos as p on (i.produto = p.id)
+                                    WHERE venda_id=@venda_id";
+
+                MySqlCommand execcmd = new MySqlCommand(cCmdSql, conn);
+
+                execcmd.Parameters.AddWithValue(@"venda_id", venda_id);
+
+
+                conn.Open();
+                execcmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(execcmd);
+                da.Fill(tabelItens);
+                conn.Close();
+
+                return tabelItens;
 
 
             }
